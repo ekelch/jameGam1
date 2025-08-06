a1cc258670f2de15f8196ac0bd80dc4c02b9765c
evankelch <evan.c.kelch@gmail.com>
2025-08-06 10:26:10 -0500

    throwing and picking up bucket

--- scenes/Level1/level_1.gd
+++ scenes/Level1/level_1.gd
@@ -1,10 +1,18 @@
 extends Node2D
-@onready var projectiles: Node2D = $Projectiles
+@onready var bucketC: Node2D = $BucketContainer
+@onready var player: CharacterBody2D = $Player
 var bucket_scene = preload("res://scenes/BucketProjectile/bucket_projectile.tscn")
+	
+func _on_player_pickup_bucket() -> void:
+	var bucketP: Vector2 = bucketC.get_child(0).position
+	var distance = bucketP.distance_to(player.position)
+	if distance < 50:
+		print('picking up')
+		bucketC.get_child(0).queue_free()
+		player.has_bucket = true
 
-func _on_player_main_action_pressed(pos: Vector2, dir: Vector2, charge: float) -> void:
+func _on_player_throw_bucket(pos: Vector2, dir: Vector2, charge: float) -> void:
 	var bucket: RigidBody2D = bucket_scene.instantiate()
 	bucket.position = pos + dir * 20
 	bucket.linear_velocity = charge * dir
-	projectiles.add_child(bucket)
-	
+	bucketC.add_child(bucket)
--- scenes/Player/player.gd
+++ scenes/Player/player.gd
@@ -2,13 +2,17 @@ extends CharacterBody2D
 
 const SPEED = 250
 const JUMP_VEL = -370
-var direction_facing = Vector2.RIGHT
 const CHARGE_MIN = 50
 const CHARGE_MAX = 800
 const CHARGE_STEP = 750
-var charge = CHARGE_MIN
 
-signal mainActionPressed(pos: Vector2, dir: Vector2, charge: float)
+var direction_facing = Vector2.RIGHT
+var charge = CHARGE_MIN
+var has_bucket: bool = true
+var charging: bool = false
+
+signal pickup_bucket
+signal throw_bucket(pos: Vector2, dir: Vector2, charge: float)
 
 func _physics_process(delta: float) -> void:
 	# gravity and jumping
@@ -25,15 +29,26 @@ func _physics_process(delta: float) -> void:
 	elif x_dir < 0:
 		$AnimatedSprite2D.animation = "move_left"
 		direction_facing = Vector2.LEFT
+	else:
+		$AnimatedSprite2D.animation = "idle_right" if direction_facing == Vector2.RIGHT else "idle_left"
 	
 	# update pos
 	position.x += x_dir * delta * SPEED
 	move_and_slide()
 	
 	# other inputs
-	if Input.is_action_pressed("main_action"):
+	if Input.is_action_just_pressed("main_action"):
+		if has_bucket:
+			charging = true
+		else:
+			charging = false
+			pickup_bucket.emit()
+		
+	if Input.is_action_pressed("main_action") && charging:
 		charge = move_toward(charge, CHARGE_MAX, delta * CHARGE_STEP)
-	elif Input.is_action_just_released("main_action"):
+	
+	if Input.is_action_just_released("main_action") && charging:
 		print("throwing with charge ", charge)
-		mainActionPressed.emit(global_position, direction_facing, charge)
+		throw_bucket.emit(global_position, direction_facing, charge)
 		charge = CHARGE_MIN
+		has_bucket = false
